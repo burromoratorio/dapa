883761ed4883d68e0220b95228259f23ff2edb2b
Alan-apiTest <amoratorio@siacseguridad.com>
2017-04-18 16:11:59 -0300

    controlador de recepcion de paquetes

---
+++ app/Http/Controllers/PacketController.php
@@ -0,0 +1,37 @@
+<?php
+/*namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Log;*/
+namespace App\Http\Controllers;
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+Use Log;
+use stdClass;
+use Storage;
+use DB;
+
+
+class PacketController extends BaseController
+{
+   public function index(Request $request) {
+    return "ok";
+   }      //
+   public function create(Request $request){
+        $method = $request->method();
+        if ($request->isMethod('post')) {
+	    $jsonReq = $request->json()->all();
+           if(isset($jsonReq["cadena"])){
+               Log::error("cadena entrante: ::".$jsonReq['cadena']);
+               return "ok";
+           }else{
+              return "ERROR:Json mal formado!";
+              Log::error("Error:json mal formado, ver palabra clave");
+           }
+        }else{
+          return "ERROR:Metodo no permitido";
+          Log::error("Error:metodo no permitido,utilizar POST");
+        }
+    }
+   
+}
