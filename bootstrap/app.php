6a001779108cad96666a40ae2dc933be5345fd8c
Alan Moratorio <amoratorio@siacseguridad.com>
2017-04-27 10:13:13 -0300

    register

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -83,7 +83,7 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
-   //$app->register(App\Providers\PuertoServiceProvider::class);
+   $app->register(App\Providers\PuertoServiceProvider::class);
    if (!class_exists('Puerto')) {
     	class_alias('App\Providers\PuertoServiceProvider', 'Puerto');
 	}
