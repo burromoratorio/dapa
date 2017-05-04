fe1d04bd2982e419652d1c277d3f269af68cbba1
Alan Moratorio <amoratorio@siacseguridad.com>
2017-05-04 12:03:00 -0300

    moviles activos row

--- app/Providers/PuertoServiceProvider.php
+++ app/Providers/PuertoServiceProvider.php
@@ -28,6 +28,7 @@ class PuertoServiceProvider extends ServiceProvider
     public static function getImei($paquete){
         self::setCadena($paquete);
         self::$moviles_activos= null;
+        self::setMovilesActivos();
         $imei="";
         if(self::$cadena!=""){
             $arrCampos = self::cadenaString2array(self::$cadena);
