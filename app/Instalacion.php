9f91c1e33dda84862ba971203a5960b1369245a0
Alan Moratorio <amoratorio@siacseguridad.com>
2017-05-04 11:25:27 -0300

    guarded

--- app/Instalacion.php
+++ app/Instalacion.php
@@ -10,7 +10,7 @@ class Instalacion extends Model
     protected $primaryKey = 'instalacion_id';
     public $timestamps = false;
     protected $fillable 	= ['equipo_id', 'localidad_id', 'movil_id'];
-
+    protected $guarded = ['movil'];
     /*public function movil() {
         return $this->hasOne('App\Movil');
     }*/
