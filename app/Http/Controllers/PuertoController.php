ce18fc98f31253910bc88f1b4e429253700bcdbc
Alan Moratorio <amoratorio@siacseguridad.com>
2017-05-05 11:39:14 -0300

    construc libre

--- app/Http/Controllers/PuertoController.php
+++ app/Http/Controllers/PuertoController.php
@@ -17,7 +17,7 @@ class PuertoController extends BaseController
     static  $moviles_activos = null;
     private function __clone() {} //Prevent any copy of this object
     private function __wakeup() {}
-    private function __construct() { 
+    private function construct() { 
         self::setMovilesActivos();   
         Log::info("new de puertoController");     
     } 
