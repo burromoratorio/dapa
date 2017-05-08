c300a5ec2042fea09c6db2fa7136dad7a7f93ec8
Alan Moratorio <amoratorio@siacseguridad.com>
2017-05-08 12:07:47 -0300

    app

--- app/Http/Controllers/PacketController.php
+++ app/Http/Controllers/PacketController.php
@@ -19,7 +19,7 @@ class PacketController extends BaseController
 	  $jsonReq = $request->json()->all();
       if(isset($jsonReq["cadena"])){
         try{
-          $imei = $this->app->Puerto->getImei($jsonReq['cadena']) ;
+          $imei = $app->Puerto->getImei($jsonReq['cadena']) ;
           Log::error("cadena entrante: ::".$jsonReq['cadena']);
           //Log::info("el imei obtenido es:".$imei);
         }catch(Exception $e){
