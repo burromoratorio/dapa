11fdca3584bb466c2f71d4108159aa06c333d7ab
Alan Moratorio <amoratorio@siacseguridad.com>
2017-06-09 13:09:48 -0300

    error de nombre de controlador

--- app/Http/Controllers/CommandController.php
+++ app/Http/Controllers/CommandController.php
@@ -8,7 +8,7 @@ use Storage;
 use DB;
 //use App\Http\Controllers\PuertoController;
 //Use Puerto;
-class AlarmController extends BaseController
+class CommandController extends BaseController
 {
   public function index(Request $request) {
     return "ok";
@@ -19,12 +19,12 @@ class AlarmController extends BaseController
 	  $jsonReq = $request->json()->all();
       if(isset($jsonReq["cadena"])){
         try{
-          app()->Puerto->analizeReport($jsonReq['cadena']) ;
-          Log::error("cadena entrante en AlarmController ::".$jsonReq['cadena']);
+          //app()->Puerto->analizeReport($jsonReq['cadena']) ;
+          Log::error("cadena entrante en CommansController ::".$jsonReq['cadena']);
         }catch(Exception $e){
           Log::error($e);
         }
-        return "ok";
+        return "$9\r\nAT+GETGP?\r\n";
       }else{
         return "ERROR:Json mal formado!";
         Log::error("Error:json mal formado, ver palabra clave");
