0559a6be10087d09884f45b507d5826b6381bf69
Alan Moratorio <amoratorio@siacseguridad.com>
2017-12-22 11:12:12 -0300

    devuelvo datyo de ejemplo hasta que este funcional el servicio

--- app/Http/Controllers/KeepAliveController.php
+++ app/Http/Controllers/KeepAliveController.php
@@ -17,7 +17,21 @@ class KeepAliveController extends BaseController
     $method = $request->method();
     if ($request->isMethod('post')) {
 	  $jsonReq = $request->json()->all();
+      $rta="";
+      $jsonReq = $request->json()->all();
       if(isset($jsonReq["cadena"])){
+        $rta  = $this->tratarReporte($jsonReq['cadena']);
+        return $rta;
+       }elseif($jsonReq["KEY"]=="KA"){
+          //por ahora devuelvo este de ejemplo
+          $rta  ="AT+GETGP?\r\n";
+        
+        return $rta;
+      }else{
+        return "ERROR:Json mal formado!";
+        Log::error("Error:json mal formado, ver palabra clave");
+      }
+      /*if(isset($jsonReq["cadena"])){
         try{
           $imei = app()->Puerto->getImei($jsonReq['cadena']) ;
           Log::error("cadena entrante: ::".$jsonReq['cadena']);
@@ -29,11 +43,21 @@ class KeepAliveController extends BaseController
       }else{
         return "ERROR:Json mal formado!";
         Log::error("Error:json mal formado, ver palabra clave");
-      }
+      }*/
     }else{
       return "ERROR:Metodo no permitido";
       Log::error("Error:metodo no permitido,utilizar POST");
     }
   }
+  public function tratarReporte($cadena){
+    $imei = "0000";
+    try{
+      $imei = app()->Puerto->getImei($cadena) ;
+      Log::error("cadena entrante en KeepAliveController ::".$cadena);
+    }catch(Exception $e){
+      Log::error($e);
+    }
+    return $imei;
+  } 
    
 }
