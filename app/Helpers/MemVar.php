6631e432a2a82d750b6583f4f7af916ec8558827
Alan Moratorio <amoratorio@siacseguridad.com>
2018-05-10 08:45:21 -0300

    no cerraba clases

--- app/Helpers/MemVar.php
+++ app/Helpers/MemVar.php
@@ -3,28 +3,29 @@ namespace App\Helpers;
 /************ class MemVar ***********************/
 class MemVar {
  
- var $identifier="";
- var $key = "";
- 
- function MemVar( $_key ) {
-  
-  $this->$key = $_key;
-  
-  $this->$identifier = @shm_attach( $_key );
- }
- 
- function setValue( $_keyvar , $_valor ) {
-  @shm_put_var( $this->$identifier , $_keyvar  , $_valor  );
- }
- 
- function getValue( $_keyvar ) {
-  return @shm_get_var( $this->$identifier , $_keyvar );
- }
- 
- function eliminar( ) {
-  @shm_remove( $this->$identifier  );
- }
- 
- function close( ) {
-  @shm_detach( $this->$identifier );
- }
+	var $identifier="";
+	var $key = "";
+
+	function MemVar( $_key ) {
+
+	$this->$key = $_key;
+
+	$this->$identifier = @shm_attach( $_key );
+	}
+
+	function setValue( $_keyvar , $_valor ) {
+	@shm_put_var( $this->$identifier , $_keyvar  , $_valor  );
+	}
+
+	function getValue( $_keyvar ) {
+	return @shm_get_var( $this->$identifier , $_keyvar );
+	}
+
+	function eliminar( ) {
+	@shm_remove( $this->$identifier  );
+	}
+
+	function close( ) {
+	@shm_detach( $this->$identifier );
+	}
+}
