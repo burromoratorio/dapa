e0f4074bde515c014d69278fa9487b24b3a1e02d
Alan Moratorio <amoratorio@siacseguridad.com>
2018-05-22 12:18:07 -0300

    test

--- app/Http/Controllers/NormalReportController.php
+++ app/Http/Controllers/NormalReportController.php
@@ -40,7 +40,7 @@ class NormalReportController extends BaseController
           $length   = $apiRta->getHeader('Content-Length');
           Log::info("length::".$length[0]);
           $largo  = (int)$length[0];
-          self::$memvar->eliminar();
+          //self::$memvar->eliminar();
           self::$memvar = new MemVar( 0,420,$largo  );
           self::$memvar->setValue( (string)$apiRta->getBody() );
           Log::info("puesto valor");
