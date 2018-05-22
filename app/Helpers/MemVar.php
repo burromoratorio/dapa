e6318a288a00c9af2d0d2a1ea77bfb74a8b893a9
Alan Moratorio <amoratorio@siacseguridad.com>
2018-05-22 15:26:12 -0300

    test

--- app/Helpers/MemVar.php
+++ app/Helpers/MemVar.php
@@ -66,8 +66,6 @@ final class MemVar
      */
     public static function Instance()
     {
-        private $author = 'Gamma, Helm, Johnson, and Vlissides';
-    	private $title  = 'Design Patterns';
         static $inst = null;
         if ($inst === null) {
             $inst = new MemVar();
@@ -88,36 +86,3 @@ final class MemVar
     	Log::info("creando singleton");
     }
 }
-/*class BookSingleton {
-    private $author = 'Gamma, Helm, Johnson, and Vlissides';
-    private $title  = 'Design Patterns';
-    private static $book = NULL;
-    private static $isLoanedOut = FALSE;
-
-    private function __construct() {
-    }
-
-    static function borrowBook() {
-      if (FALSE == self::$isLoanedOut) {
-        if (NULL == self::$book) {
-           self::$book = new BookSingleton();
-        }
-        self::$isLoanedOut = TRUE;
-        return self::$book;
-      } else {
-        return NULL;
-      }
-    }
-
-    function returnBook(BookSingleton $bookReturned) {
-        self::$isLoanedOut = FALSE;
-    }
-
-    function getAuthor() {return $this->author;}
-
-    function getTitle() {return $this->title;}
-
-    function getAuthorAndTitle() {
-      return $this->getTitle() . ' by ' . $this->getAuthor();
-    }
-  }*/
