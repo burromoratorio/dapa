2c345f645c5fbce748952b5cb47254110c5f54e3
Alan Moratorio <amoratorio@siacseguridad.com>
2018-05-22 08:31:25 -0300

    pruebas

--- app/Http/Controllers/NormalReportController.php
+++ app/Http/Controllers/NormalReportController.php
@@ -36,7 +36,7 @@ class NormalReportController extends BaseController
         if($code=="200" && $reason=="OK"){
           Log::error("Moviles en api:::".(string)$apiRta->getBody());
           //cantidad de octetos en la rta, es decir rta*8=xbits==es decir son los bytes
-          $length   = $apiRta->getHeader('Content-Length');
+         /* $length   = $apiRta->getHeader('Content-Length');
           Log::info("length::".$length[0]);
           $memvar = new MemVar( 100,420, $length[0] );
           $memvar->setValue( (string)$apiRta->getBody() );
@@ -45,7 +45,7 @@ class NormalReportController extends BaseController
           //Note: the 3rd and 4th should be entered as 0 if you are opening an existing memory segment. 
           $memvar = new MemVar( 100,0,0 );
           Log::info( "valor = ".$memvar->getValue());
-          $memvar->close();
+          $memvar->close();*/
         }else{
           Log::error("Bad Response :: code:".$code." reason::".$reason);
         }
