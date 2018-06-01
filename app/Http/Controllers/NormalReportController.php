bb5a40601794ab411fb08f206bedfc116a2df706
Alan Moratorio <amoratorio@siacseguridad.com>
2018-06-01 09:02:53 -0300

    prueba de busqueda de imei con funcion estatica del puerto

--- app/Http/Controllers/NormalReportController.php
+++ app/Http/Controllers/NormalReportController.php
@@ -20,7 +20,7 @@ class NormalReportController extends BaseController
   /*
     desde MONA ingresa con $jsonReq["cadena"]
     desde Puerto_lite con ($jsonReq["KEY"]
-  */   // probar con este imei 863835020075979
+  */  
   public function create(Request $request){
     
     $method = $request->method();
@@ -29,13 +29,13 @@ class NormalReportController extends BaseController
       $jsonReq = $request->json()->all();
       //Log::error(print_r($jsonReq, true));
       if(isset($jsonReq["cadena"])){
+        //pruebas en obtencion de imei del json
+        $arrCadena = app()->Puerto::changeString2array($jsonReq["cadena"]);
+        Log::info("se obtuvo este IMEI::".$arrCadena['IMEI']);
         $apiRta   = $this->obtenerMoviles();
         $code     = $apiRta->getStatusCode(); 
         $reason   = $apiRta->getReasonPhrase();
         if($code=="200" && $reason=="OK"){
-          //Log::error("Moviles en api:::".(string)$apiRta->getBody());
-          //cantidad de octetos en la rta, es decir rta*8=xbits==es decir son los bytes
-          //$apiRta->getHeader('Content-Length');
           $body     = $apiRta->getBody();
           $length   = strlen($apiRta->getBody());
           Log::error("Content-Length:::".strlen($apiRta->getBody()));
@@ -65,26 +65,7 @@ class NormalReportController extends BaseController
             $memvar->setValue( $body );
             */
           }
-          //MemVar::VaciaMemoria();
-          //MemVar::GetValue();
-          //MemVar::Eliminar();
-          /*$memvar = MemVar::Instance();
-          if( $memvar->init(0,$largo) ){
-            MemVar::OpenToRead();
-            $memvar->setValue( (string)$apiRta->getBody() );
-            MemVar::GetValue();
-            //MemVar::Eliminar();
-          }else{
-
-          }*/
-          // new MemVar( 0,420,$largo  );
-          //$memvar->setValue( (string)$apiRta->getBody() );
-          //Log::info("puesto valor");
-          //$memvar->close();
-          //Note: the 3rd and 4th should be entered as 0 if you are opening an existing memory segment. 
-          //$memvar = new MemVar( 100,0,0 );
-          //Log::info( "valor = ".$memvar->getValue());
-          //$memvar->close();
+          
         }else{
           Log::error("Bad Response :: code:".$code." reason::".$reason);
         }
@@ -131,16 +112,6 @@ class NormalReportController extends BaseController
       $rta  = '0';
     }
     return $rta;
-    //863835020075979
-    //Note: the 3rd and 4th should be entered as 0 if you are opening an existing memory segment. 
-    //$memvar       = new MemVar( 100,0,0 );
-    //$jsonMoviles  = json_decode($memvar->getValue());
-    //var_dump($jsonMoviles);
-    /*foreach ($jsonMoviles as $movil) {
-      Log::info( "valor compruebaMovilMC = ".print_r($movil, true));
-    }*/
-    
-    //$memvar->close();
     
   }
   /*public static function dameMoviles(){
