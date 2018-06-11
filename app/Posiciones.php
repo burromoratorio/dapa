8445c2cc3f225458da55ba9e92e7bea0a5f8cb51
Alan Moratorio <amoratorio@siacseguridad.com>
2018-06-11 11:25:53 -0300

    test

--- app/Posiciones.php
+++ app/Posiciones.php
@@ -9,7 +9,7 @@ class Posiciones extends Model
     protected $table = 'POSICIONES';
     public $timestamps = false;
     protected $connection = 'siac';
-    protected $primaryKey = ['posicion_id'];    
+    protected $primaryKey = 'posicion_id';    
     protected $fillable = array('movil_id','cmd_id','tipo', 'fecha','rumbo_id', 'fecha', 'latitud','longitud',
                             'velocidad','valida','estado_u','estavo_v','estado_w','km_recorridos','referencia',
                             'ltrs_consumidos','ltrs_100','rpm','ancho_pasada');
