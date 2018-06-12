3cee309e369a2e19dfd76892cfc701b725d36541
Alan Moratorio <amoratorio@siacseguridad.com>
2018-06-12 12:55:12 -0300

    test

--- app/Http/Controllers/PuertoController.php
+++ app/Http/Controllers/PuertoController.php
@@ -225,7 +225,7 @@ class PuertoController extends BaseController
                 // Driver code
                 $memoMoviles    = MemVar::GetValue();
                 $memoMoviles    = json_decode($memoMoviles);
-                $encontrado         = self::binarySearch($memoMoviles, 0, count($memoMoviles) - 1, 359231037704523);
+                $encontrado         = self::binarySearch($memoMoviles, 0, count($memoMoviles) - 1, 351687032250002);
                 if($encontrado== false) {
                     Log::info("NOOOO Existeeeees");
                 }
