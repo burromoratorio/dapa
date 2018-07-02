2c0def37da6df00a825b8f3a2571dcb02d89033c
Alan Moratorio <amoratorio@siacseguridad.com>
2018-07-02 09:02:20 -0300

    log pruebas nueva fecha

--- app/Http/Controllers/PuertoController.php
+++ app/Http/Controllers/PuertoController.php
@@ -120,6 +120,7 @@ class PuertoController extends BaseController
     public static function ddmmyy2yyyymmdd($fecha,$hora){
         $formatFecha = date("Y-m-d h:i:s", mktime(substr($hora, 0,2), substr($hora, 2,2), substr($hora, 4,2), substr($fecha, 2,2), substr($fecha, 0,2), substr($fecha, -2,2)));
         $nuevafecha = strtotime ( '-3 hours' , strtotime ( $formatFecha ) ) ;
+        Log::info("fecha nueva:".$nuevafecha);
         $nuevafecha = date ( 'Y-m-d h:i:s' , $nuevafecha );
          return $nuevafecha;
     }
