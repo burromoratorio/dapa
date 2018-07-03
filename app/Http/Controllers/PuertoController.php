7e494e2e02946d30dafb0f13606408909a8e57f1
Alan Moratorio <amoratorio@siacseguridad.com>
2018-07-03 12:39:08 -0300

    pruebas error en array de posiciones

--- app/Http/Controllers/PuertoController.php
+++ app/Http/Controllers/PuertoController.php
@@ -122,6 +122,7 @@ class PuertoController extends BaseController
             $posicionesMC   = [];
             MemVar::initIdentifier($shmidPos);
             $memoPos    = MemVar::GetValue();
+            Log::info($memoPos);
             $posArr     = json_encode($memoPos);
             foreach ($posArr as $key => $value) {
                 array_push($posicionesMC, $value);
