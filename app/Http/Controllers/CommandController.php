77078c5410ea2a8a75d0afb06742779700b6a12f
Alan Moratorio <amoratorio@siacseguridad.com>
2018-09-11 08:32:35 -0300

    envio y recepcion de comandos con codigos

--- app/Http/Controllers/CommandController.php
+++ app/Http/Controllers/CommandController.php
@@ -8,14 +8,22 @@ Use Log;
 use stdClass;
 use Storage;
 use DB;
-use App\GprmcComando;
-//use App\Http\Controllers\PuertoController;
-//Use Puerto;
+/*DDBB Principal*/
+use App\ColaMensajes;
+/*Helpers*/
+use App\Helpers\MemVar;
+use GuzzleHttp\Client;
+
 class CommandController extends BaseController
 {
   const ESTADO_PENDIENTE = 1;
   const ESTADO_EN_PROCESO = 2;
   const ESTADO_OK = 3;
+  const POSICION_ACTUAL=17;
+  const CONFIGURAR_FRECUENCIAS=20;
+  const CAMBIO_MODO_PRESENCIA_GPRS=22;
+  const RESET_GPRMC=100;
+  static $comandoDefinitions = array("+GETGP"=>17,"+FR"=>20,"+AVL"=>20,"+RES"=>100,"+OUTS"=>22);
   public function index(Request $request) {
     //return "ok";
   }      //
@@ -137,17 +145,86 @@ class CommandController extends BaseController
     }
     public function update(Request $request, $imei) {
         $comandoRta = $request->input('rta');
-        $estado   = $request->input('estado_comando_id');
-        Log::info('recibido en dapa, IMEI:'.$imei.' rta:'.$comandoRta.' estado:'.$estado);
-        //$comando  = GprmcComando::findOrFail($imei);
-        
-        /*if ($estado == static::ESTADO_EN_PROCESO) {
-            
+        $estado     = $request->input('estado_comando_id');
+        $arrCmdRta  = explode(":",$comandoRta);
+        //$comandoDefinitions
+        Log::info('recibido en dapa, IMEI:'.$imei.' rta:'.$comandoRta.' estado:'.$estado.' se obtuvo:'.$arrCmdRta[0]." de CMD_ID:".self::$comandoDefinitions[$arrCmdRta[0]]);
+        $movil    = $this->movilesMemoria($imei);
+        $equipo_id= $movil->equipo_id;
+        $mensaje  = ColaMensajes::where('modem_id', '=',$equipo_id)
+                                ->where('rsp_id','=',2)
+                                ->where('cmd_id','=',self::$comandoDefinitions[$arrCmdRta[0]])
+                                ->orderBy('prioridad','DESC')
+                                ->get()->first(); 
+        if(!is_null($mensaje)){
+          $mensaje->rsp_id      = 3;
+          $mensaje->respuesta   = $comandoRta;
+          $mensaje->fecha_final = date("Y-m-d H:i:s");
+          $mensaje->save();
+          return "Update OK";
+        }else{
+          Log::info("No existe comando pendiente");
         }
-        $comando->status    = $estado;
-        $comando->fecha_rta = $estado;
-        $comando->save();*/
-        return "Update OK";
+        
+        
     }
+    public function compruebaMovilMC($imei,$shmid){
+    //Movil Binary Search 
+    MemVar::initIdentifier($shmid);
+    $memoMoviles    = MemVar::GetValue();
+    $memoMoviles    = json_decode($memoMoviles);
+    $encontrado     = app()->Puerto::binarySearch($memoMoviles, 0, count($memoMoviles) - 1, $imei);
+    return $encontrado;
+    
+  } 
+  protected function obtenerMoviles() {
+      Log::error("Buscando moviles en code.siacseguridad.com");
+      // Create a client with a base URI
+      $client = new Client(['base_uri' => 'http://code.siacseguridad.com:8080/api/']);
+      // Send a request to https://foo.com/api/test
+      $response = $client->request('GET', 'equipos/1');
+
+      return $response;
+  }
+  public function movilesMemoria($imei){
+    Log::error(print_r($imei, true));
+    $requestApi   = '0';
+    $mcRta        = '0';
+    $mcRta2       = '0';
+    $movil        = false;
+    $shmid        = MemVar::OpenToRead('moviles.dat');
+    if($shmid!='0'){
+        Log::info("Verificando validez IMEI ".$imei);
+        $mcRta        = $this->compruebaMovilMC($imei,$shmid);
+      if($mcRta==false){
+        Log::info("El IMEI ".$imei." no está en la memoria");
+        $requestApi= '1';
+      }else{
+        $mcRta2    = '1';
+        $movil     = $mcRta;
+        Log::info("::::::::Procesando:".$imei."- Movil_id:".$movil->movil_id."-MovilOld_id:".$movil->movilOldId."::::::::");
+      }
+    }else{
+        $requestApi   = '1';
+        Log::info("No existe el shmid->voy a crear nuevo segmento");
+    }
+    if($requestApi   == '1'){
+        $apiRta   = $this->obtenerMoviles();
+        if($apiRta->getStatusCode()=="200" && $apiRta->getReasonPhrase()=="OK"){
+          $length   = strlen($apiRta->getBody());
+          $largo    = (int)$length;
+          //Log::error("Content-Length:::".strlen($apiRta->getBody()));
+          MemVar::VaciaMemoria();
+          $memvar = MemVar::Instance('moviles.dat');
+          $memvar->init('moviles.dat',$largo);
+          $memvar->setValue( $apiRta->getBody() );
+          $shmid  = MemVar::OpenToRead('moviles.dat');
+          $movil  = $this->compruebaMovilMC($imei,$shmid);
+        }else{
+          Log::error("Bad Response :: code:".$code." reason::".$reason);
+        }
+    }
+  return $movil;
+  }
    
 }
--- app/Http/Controllers/KeepAliveController.php
+++ app/Http/Controllers/KeepAliveController.php
@@ -6,58 +6,172 @@ Use Log;
 use stdClass;
 use Storage;
 use DB;
-//use App\Http\Controllers\PuertoController;
-//Use Puerto;
+/*DDBB Principal*/
+use App\ColaMensajes;
+/*Helpers*/
+use App\Helpers\MemVar;
+use GuzzleHttp\Client;
 class KeepAliveController extends BaseController
 {
+  const EN_MOVIMIENTO = 1;
+  const EN_DETENIDO   = 0;
   public function index(Request $request) {
     return "ok";
   }      //
   public function create(Request $request){
-    $method = $request->method();
+    $method   = $request->method();
+    $comando  = "";
     if ($request->isMethod('post')) {
-	  $jsonReq = $request->json()->all();
-      $rta="";
-      $jsonReq = $request->json()->all();
+      $jsonReq      = $request->json()->all();
       if(isset($jsonReq["cadena"])){
-        $rta  = $this->tratarReporte($jsonReq['cadena']);
-        return $rta;
-      }elseif($jsonReq["KEY"]=="KA"){
-          //por ahora devuelvo este de ejemplo
-          $rta  ="AT+GETGP?\r\n";
-        
-        return $rta;
+        $movil    = $this->movilesMemoria($jsonReq["cadena"]);
+        //$rta      = $movil->equipo_id;
+        $mensaje  = $this->obtenerComandoPendiente($movil->equipo_id); 
+        if($mensaje){
+          if($mensaje->comando!=''&& !is_null($mensaje->comando)){
+            $comando="AT".$mensaje->comando.'='.$mensaje->auxiliar;
+          }else{
+              $comando  = $this->decodificarComando($mensaje,$movil);
+          }
+        }else{
+          $comando  ="AT+GETGP?\r\n"; 
+        }
+        Log::info("comandeando:".$comando);
+       }elseif($jsonReq["KEY"]=="KA"){
+        //por ahora devuelvo este de ejemplo
+        $comando  ="AT+GETGP?\r\n";
       }else{
         return "ERROR:Json mal formado!";
         Log::error("Error:json mal formado, ver palabra clave");
       }
-      /*if(isset($jsonReq["cadena"])){
-        try{
-          $imei = app()->Puerto->getImei($jsonReq['cadena']) ;
-          Log::error("cadena entrante: ::".$jsonReq['cadena']);
-          Log::info("el imei obtenido es:".$imei);
-        }catch(Exception $e){
-          Log::error($e);
-        }
-        return "ok";
-      }else{
-        return "ERROR:Json mal formado!";
-        Log::error("Error:json mal formado, ver palabra clave");
-      }*/
-    }else{
-      return "ERROR:Metodo no permitido";
-      Log::error("Error:metodo no permitido,utilizar POST");
     }
+    return  $comando;
   }
-  public function tratarReporte($cadena){
-    $imei = "0000";
+  public function tratarReporte($cadena,$movil){
+    $rta  = "";
     try{
-      $imei = app()->Puerto->getImei($cadena) ;
-      Log::error("cadena entrante en KeepAliveController ::".$cadena);
+      Log::error("Equipo=>".$movil->equipo_id." MOVIL=>".$movil->movilOldId."-Cadena=>".$cadena);
+      $rta  = app()->Puerto->analizeReport($cadena,$movil) ;
     }catch(Exception $e){
+      $rta  = "error";
       Log::error($e);
     }
-    return $imei;
+    return $rta;
   } 
-   
+  public function compruebaMovilMC($imei,$shmid){
+    //Movil Binary Search 
+    MemVar::initIdentifier($shmid);
+    $memoMoviles    = MemVar::GetValue();
+    $memoMoviles    = json_decode($memoMoviles);
+    $encontrado     = app()->Puerto::binarySearch($memoMoviles, 0, count($memoMoviles) - 1, $imei);
+    return $encontrado;
+    
+  } 
+  protected function obtenerMoviles() {
+      Log::error("Buscando moviles en code.siacseguridad.com");
+      // Create a client with a base URI
+      $client = new Client(['base_uri' => 'http://code.siacseguridad.com:8080/api/']);
+      // Send a request to https://foo.com/api/test
+      $response = $client->request('GET', 'equipos/1');
+
+      return $response;
+  }
+  public function movilesMemoria($imei){
+    Log::error(print_r($imei, true));
+    $requestApi   = '0';
+    $mcRta        = '0';
+    $mcRta2       = '0';
+    $movil        = false;
+    $shmid        = MemVar::OpenToRead('moviles.dat');
+    if($shmid!='0'){
+        Log::info("Verificando validez IMEI ".$imei);
+        $mcRta        = $this->compruebaMovilMC($imei,$shmid);
+      if($mcRta==false){
+        Log::info("El IMEI ".$imei." no está en la memoria");
+        $requestApi= '1';
+      }else{
+        $mcRta2    = '1';
+        $movil     = $mcRta;
+        Log::info("::::::::Procesando:".$imei."- Movil_id:".$movil->movil_id."-MovilOld_id:".$movil->movilOldId."::::::::");
+      }
+    }else{
+        $requestApi   = '1';
+        Log::info("No existe el shmid->voy a crear nuevo segmento");
+    }
+    if($requestApi   == '1'){
+        $apiRta   = $this->obtenerMoviles();
+        if($apiRta->getStatusCode()=="200" && $apiRta->getReasonPhrase()=="OK"){
+          $length   = strlen($apiRta->getBody());
+          $largo    = (int)$length;
+          //Log::error("Content-Length:::".strlen($apiRta->getBody()));
+          MemVar::VaciaMemoria();
+          $memvar = MemVar::Instance('moviles.dat');
+          $memvar->init('moviles.dat',$largo);
+          $memvar->setValue( $apiRta->getBody() );
+          $shmid  = MemVar::OpenToRead('moviles.dat');
+          $movil  = $this->compruebaMovilMC($imei,$shmid);
+        }else{
+          Log::error("Bad Response :: code:".$code." reason::".$reason);
+        }
+    }
+  return $movil;
+  }
+  public function obtenerComandoPendiente($equipo_id){
+    $mensaje  = false;
+    $mensaje  = ColaMensajes::where('modem_id', '=',$equipo_id)
+                                ->where('rsp_id','=',1)->orderBy('prioridad','DESC')
+                                ->get()->first(); 
+    $mensaje->rsp_id      = 2;
+    $mensaje->fecha_final = date("Y-m-d H:i:s");
+    $mensaje->save();
+    return $mensaje;
+  }
+  public function decodificarComando($mensaje,$movil){
+    
+    //falta guardar en memoria el estado de velocidad max del equipo y tiempo de veloc max
+    $auxParams  = explode(",",$mensaje->auxiliar);
+    switch ($mensaje->cmd_id) {
+      case 17:
+      Log::info("entra por 17");
+        $cadenaComando  = "+GETGP";
+        break;
+      case 20://frecuencias y velocidades
+      Log::info("entra por 20");
+        switch ($auxParams[1]) {
+          case '6':
+            $cadenaComando = "FR=0,".self::EN_MOVIMIENTO.",".$auxParams[2];
+            break;
+          case '7':
+            $cadenaComando = "FR=0,".self::EN_DETENIDO.",".$auxParams[2];
+            break;
+            case '20':
+            //seteo tiempo de reporte en veloc max ALV=t,v
+            $cadenaComando = "ALV=".$movil->velocidad_max.",".$auxParams[2];
+            break;
+            case '23':
+            //seteo velocidad max max ALV=t,v
+            $cadenaComando = "ALV=".$auxParams[2].",".intval($movil->frec_rep_exceso_vel*60);
+            break;
+          default:
+            # code...
+            break;
+        }
+        break;
+      case 22://modo corte(aux=1,2->0,1) y modo normal(aux=1,1->0,0)
+        Log::info("entra por 22 y auxParams:".$auxParams[1]);
+        $valor  = ($auxParams[1]=="2")?"1":"0";
+        $cadenaComando = "OUTS=0,".$valor;
+        break;
+      case 100://reset
+      Log::info("entra por 100");
+        $cadenaComando = "RES=".$auxParams[0];
+        break;
+      default:
+      Log::info("entra por default");
+        $cadenaComando  = "+GETGP?";
+        break;
+    }
+    Log::info("cadenaComando:".$cadenaComando);
+    return $cadenaComando;
+  }
 }
