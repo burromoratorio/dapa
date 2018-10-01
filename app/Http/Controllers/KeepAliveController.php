ae18129103afa3335e9a072d6600c2f9daa6b344
Alan Moratorio <amoratorio@siacseguridad.com>
2018-10-01 14:47:31 -0300

    test

--- app/Http/Controllers/KeepAliveController.php
+++ app/Http/Controllers/KeepAliveController.php
@@ -28,12 +28,12 @@ class KeepAliveController extends BaseController
         $movil    = HelpMen::movilesMemoria($jsonReq["cadena"]);
         //$rta      = $movil->equipo_id;
         $mensaje  = $this->obtenerComandoPendiente($movil->equipo_id); 
-        Log::info("mensaje->comando:".$mensaje->comando." mensaje->auxiliar:".$mensaje->auxiliar);
         if($mensaje){
           if($mensaje->comando!='' && !is_null($mensaje->comando)){
             $valorSet   = (isset($mensaje->auxiliar) && !is_null($mensaje->auxiliar) && $mensaje->auxiliar!='')?'='.$mensaje->auxiliar:"?";
             $comando="AT".$mensaje->comando.$valorSet."\r\n";
           }else{
+        Log::info("mensaje->comando:".$mensaje->comando." mensaje->auxiliar:".$mensaje->auxiliar);
             $comando="AT".$this->decodificarComando($mensaje,$movil)."\r\n";
           }
         }else{
