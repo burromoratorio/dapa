fbfa9563ddf14a69f161e56f1dddb7bc3f6c0534
Alan Moratorio <amoratorio@siacseguridad.com>
2018-12-04 08:09:28 -0300

    pruebas alarmnas bateria

--- app/Http/Controllers/PuertoController.php
+++ app/Http/Controllers/PuertoController.php
@@ -371,7 +371,9 @@ class PuertoController extends BaseController
                 Log::error("ESTADO SENSORES ALARMA BAT:::posicion::".$io." Memoria::".$sensorEstado->io." MOVIL::.".$imei);
                 DB::beginTransaction();
                 try {
-                    EstadosSensores::where('imei', $imei)->update(['io' => '11111']);
+                    //EstadosSensores::where('imei', $imei)->update(['io' => '11111']);
+                    DB::table('estados_sensores')->where('imei', $imei)->update(['io' => '11111']);
+
                     DB::commit();
                     self::persistSensor($ioData,$imei,$posicion_id,$movilOldId,$movil_id,$fecha,$tipo_alarma_id,$estado_movil_id);
                 }catch (\Exception $ex) {
