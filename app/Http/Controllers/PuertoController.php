6c2d15826cbaf6ef4250f6ac4f87b98fc179e605
Alan Moratorio <amoratorio@siacseguridad.com>
2019-06-13 07:55:01 -0300

    test

--- app/Http/Controllers/PuertoController.php
+++ app/Http/Controllers/PuertoController.php
@@ -403,6 +403,7 @@ class PuertoController extends BaseController
                       
                 $movTestings = array(10004, 10006, 10033);
                 if (in_array($movil->equipo_id, $movTestings)){
+                    Log::info("por enviar al api de nacho el movil:".$movil->equipo_id);
                     /*$json=  ["movil"=>intval($movil->movilOldId),
                         "point"=> ["type"=>"Point","coordinates"=> [$arrInfoGprmc['longitud'],$arrInfoGprmc['latitud'] ] ],
                         "received"=>$fecha, "speed"=> $arrInfoGprmc['velocidad'], "direction"=>$arrInfoGprmc['rumbo']
