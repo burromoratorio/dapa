7904060bb372b4bd10e524c4c3dc31961285845d
Alan Moratorio <amoratorio@siacseguridad.com>
2019-07-05 15:01:46 -0300

    arreglo sintaxis

--- app/Http/Controllers/NormalReportController.php
+++ app/Http/Controllers/NormalReportController.php
@@ -54,10 +54,12 @@ class NormalReportController extends BaseController
               $movilFicticio->movilOldId = "";$movilFicticio->estado_u = "";
               array_push($memoMoviles, $movilFicticio);
               $movilesForMemo = json_encode($memoMoviles);
+              $length         = strlen($movilesForMemo);
+              $largo          = (int)$length;
               //Log::info($movilesForMemo);
               MemVar::VaciaMemoria();
               $memvar = MemVar::Instance('moviles.dat');
-              $memvar->init('moviles.dat',$largo+1);
+              $memvar->init('moviles.dat',$largo);
               $memvar->setValue( $movilesForMemo );
               $shmid  = MemVar::OpenToRead('moviles.dat');
             }
