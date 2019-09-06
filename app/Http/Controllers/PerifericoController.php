3f1e44bd73b5db21efd7bd274a71bf60674ca5f9
Alan Moratorio <amoratorio@siacseguridad.com>
2019-09-06 11:51:32 -0300

    nuevas funciones y cosas en clases para actualizar sensores

--- app/Http/Controllers/PerifericoController.php
+++ app/Http/Controllers/PerifericoController.php
@@ -23,14 +23,15 @@ class PerifericoController extends BaseController
     public static function setSensores($equipo_id,$sensores){
         DB::beginTransaction();
         try{
+            dd($perif);
             $perif = self::getSensores($equipo_id);
             $perif->sensor_pulsador_panico=$sensores[0];
             $perif->sensor_puerta_conductor=$sensores[1];
             $perif->sensor_puerta_acompaniante=$sensores[2];
-            $perif->PERIF_IO.sensor_desenganche=$sensores[3];
-            $perif->PERIF_IO.sensor_antisabotaje=$sensores[4];
+            //$perif->PERIF_IO->sensor_desenganche=$sensores[3];
+            //$perif->PERIF_IO->sensor_antisabotaje=$sensores[4];
             $perif->sensor_compuerta=$sensores[5];
-            $perif->PERIF_IO.sensor_encendido=$sensores[7];
+            //$perif->PERIF_IO->sensor_encendido=$sensores[7];
             $perif->save();
             Log::info("Actualizando PerifericoController:::");
 
