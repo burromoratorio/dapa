e6a0e9362d9ab1b2b1ac91ad3daf0700420ccd58
Alan Moratorio <amoratorio@siacseguridad.com>
2019-09-10 14:27:30 -0300

    test

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -238,6 +238,7 @@ class SensorController extends BaseController {
     /*I4: Desenganche=>0 = ENGANCHADO; 1 = DESENGANCHADO | I5: Antisabotaje=>0 = VIOLACION; 1 = NORMAL | I6: Compuerta=>0 = CERRADA; 1 = ABIERTA*/
     public static function cambiosInputIOM($imei,$iomArr,$sensorEstado,$movil,$estado_movil_id){
         $rta         = array("rta"=>0,"estado_movil_id"=>$estado_movil_id,"tipo_alarma_id"=>0); //alarma_id=7 (Normal)
+        Log::info("CAMBIOS INPUT OUTPUTTTTTT");
         self::actualizarPerifericos($movil,$iomArr);
         if($sensorEstado && $sensorEstado->iom){
             $estadoArr = str_split($sensorEstado->iom);
@@ -354,7 +355,7 @@ class SensorController extends BaseController {
     
     }
     public static function actualizarPerifericos($movil,$estadoArr){
-        Log::info("Actualizando datos de Perifericos:::");
+        Log::info(":::::::Actualizando datos de Perifericos:::");
         try{
             PerifericoController::setSensores($movil->equipo_id,$estadoArr);
         }catch (\Exception $ex) {
