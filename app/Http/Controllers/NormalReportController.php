adccf16d2c05dcb0fe31ff8fd8e9c6a4fd1ecfb1
Alan Moratorio <amoratorio@siacseguridad.com>
2019-09-13 12:53:11 -0300

    escapo cadena sin imei

--- app/Http/Controllers/NormalReportController.php
+++ app/Http/Controllers/NormalReportController.php
@@ -87,7 +87,7 @@ class NormalReportController extends BaseController
         if($movil!=false){
           $rta    = $this->tratarReporte($jsonReq['cadena'],$movil);
         }else{
-         Log::error("El IMEI:".$arrCadena['IMEI']." No esta en la DDBB-->desecho reporte");
+         Log::error("El IMEI:".(isset($arrCadena['IMEI']))?$arrCadena['IMEI']:"0"." No esta en la DDBB-->desecho reporte");
         }
         
       }elseif($jsonReq["KEY"]=="NR"){
