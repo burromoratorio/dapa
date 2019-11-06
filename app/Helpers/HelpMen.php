c1cfff4865944c3cf2c515234ffbbe924a4e39b2
Alan Moratorio <amoratorio@siacseguridad.com>
2019-11-06 11:40:49 -0300

    limpio el helpmen

--- app/Helpers/HelpMen.php
+++ app/Helpers/HelpMen.php
@@ -26,18 +26,6 @@ class HelpMen
     const OFFSET_VELOCIDAD= 6;
     const OFFSET_RUMBO  = 7;
     
-    public static function compruebaMovilMC($imei,$shmid){
-        //Movil Binary Search 
-        $encontrado=false;
-        MemVar::initIdentifier($shmid);
-        $memoMoviles    = MemVar::GetValue();
-        $memoMoviles    = json_decode($memoMoviles);
-        if($memoMoviles!=''){
-            $encontrado     = self::binarySearch($memoMoviles, 0, count($memoMoviles) - 1, $imei);
-        }
-       
-        return $encontrado;
-    } 
     public static function obtenerMoviles() {
         Log::error("Buscando moviles en code.siacseguridad.com");
         // Create a client with a base URI
@@ -46,45 +34,7 @@ class HelpMen
         $response = $client->request('GET', 'equipos/1');
         return $response;
     }
-    public static function movilesMemoria($imei){
-        $requestApi   = '0';
-        $mcRta        = '0';
-        $mcRta2       = '0';
-        $movil        = false;
-        $shmid        = MemVar::OpenToRead('moviles.dat');
-        if($shmid!='0'){
-            Log::info("Verificando validez IMEI ".$imei);
-            $mcRta        = self::compruebaMovilMC($imei,$shmid);
-          if($mcRta==false){
-            Log::info("El IMEI ".$imei." no está en la memoria");
-            $requestApi= '1';
-          }else{
-            $mcRta2    = '1';
-            $movil     = $mcRta;
-            Log::info("::::::::Procesando:".$imei."- Movil_id:".$movil->movil_id."-MovilOld_id:".$movil->movilOldId."::::::::");
-          }
-        }else{
-            $requestApi   = '1';
-            Log::info("No existe el shmid->voy a crear nuevo segmento");
-        }
-        if($requestApi   == '1'){
-            $apiRta   = self::obtenerMoviles();
-            if($apiRta->getStatusCode()=="200" && $apiRta->getReasonPhrase()=="OK"){
-              $length   = strlen($apiRta->getBody());
-              $largo    = (int)$length;
-              //Log::error("Content-Length:::".strlen($apiRta->getBody()));
-              MemVar::VaciaMemoria();
-              $memvar = MemVar::Instance('moviles.dat');
-              $memvar->init('moviles.dat',$largo);
-              $memvar->setValue( $apiRta->getBody() );
-              $shmid  = MemVar::OpenToRead('moviles.dat');
-              $movil  = self::compruebaMovilMC($imei,$shmid);
-            }else{
-              Log::error("Bad Response :: code:".$code." reason::".$reason);
-            }
-        }
-      return $movil;
-    }
+    
     public static function report($archivo,$logdata) {
             $logstring	= "[".date('Y-m-d H:i:s')."]".$logdata;
         file_put_contents(storage_path('logs/equipo_'.$archivo.'_'.date('Y-m-d').'.log'), (string) $logstring, FILE_APPEND);
@@ -124,14 +74,7 @@ class HelpMen
             return self::binarySearch($arr, $mid + 1, $end, $x);
         }
     }
-    public static function CargarMemoria($archivo,$dataArray){
-        $memvar     = MemVar::Instance($archivo);
-        $enstring   = json_encode($dataArray);
-        $largo      = (int)strlen($enstring);
-        $memvar->init($archivo,$largo);
-        $memvar->setValue( $enstring );
-    }
-    /*
+     /*
      * 
      * Nuevo entorno con redis
      *
