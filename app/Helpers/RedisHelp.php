8e017abff8f1028aaf69ca3e873b11d267c1fe85
Alan Moratorio <amoratorio@siacseguridad.com>
2019-11-06 11:47:57 -0300

    prueba con json enconde en lookformovil

--- app/Helpers/RedisHelp.php
+++ app/Helpers/RedisHelp.php
@@ -107,6 +107,6 @@ class RedisHelp {
         }else{
             Log::error("el movil no esta en REDIS::::::");
         }
-        return json_encode($movil);
+        return json_decode(json_encode($movil), FALSE); 
     }
 }
