afbd81e30cc5ae5c651d38a3ad8970cb9fca3db3
Alan Moratorio <amoratorio@siacseguridad.com>
2020-01-20 11:36:57 -0300

    log iomarr pruebas con antisabotaje

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -138,9 +138,6 @@ class SensorController extends BaseController {
                 Log::error("ANTES DE LA PREGUNTAAAAAAAAAAAAAA_::::::::::::::");
                 Log::error(print_r($sensorEstado,true));
                 if(is_null($sensorEstado) || $sensorEstado=='' ){
-                    Log::error("::::::::::::::::se vino aca::::::::::::::::::::::::");
-                    log::error("VA A ACTUALIZAR EN REDIS::::::::::::::::");
-            Log::error("imeiiii:::".$movil->imei." perfield::::::".$perField);
                     $sensorNuevo  = EstadosSensores::where('movil_id', '=',$movil->movil_id)->orderBy('updated_at','DESC')->first();
                     if(!$sensorNuevo){//si no tenia en la ddbb data
                         HelpMen::report($movil->equipo_id,"Datos de sensores vacios en DDBB, generando...");
@@ -155,6 +152,9 @@ class SensorController extends BaseController {
                         }
                         self::persistSensor($posicion_id,$movil,$fecha,$rta["tipo_alarma_id"],$rta["estado_movil_id"]);
                     }else{//si tenía pero posiblemente solo de IO, genero el IOM
+                        Log::error("::::::::::::::::se vino aca::::::::::::::::::::::::");
+                        log::error("VA A ACTUALIZAR EN REDIS::::::::::::::::");
+                        Log::error("imeiiii:::".$movil->imei." perfield::::::".$perFieldInput);
                         HelpMen::report($movil->equipo_id,"Actualizando datos de IOM en instalacion que antes tenia IO");
                         self::updateSensores($movil,$perFieldInput,"",$rta["tipo_alarma_id"],$rta["estado_movil_id"],$posicion_id,$fecha);
                         //poner tambien el actualiza perifericos
