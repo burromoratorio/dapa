be515319b326e24ee69663312de8b3588f7ad3c8
Alan Moratorio <amoratorio@siacseguridad.com>
2020-03-09 11:37:09 -0300

    logs de pruebass

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -231,8 +231,9 @@ class SensorController extends BaseController {
                     HelpMen::report($movil->equipo_id,"Actualizando datos de IOM en instalacion que antes tenia IO \r\n");
                     self::actualizarPerifericos($movil,$arrPer,$perFieldOutput,$manualRestartMethod);
                     $rta = PerifericoHelp::cambiosInputIOM($arrPer,$movil->iom,$movil,$rta["estado_movil_id"],$perFieldOutput,$manualRestartMethod);
+                    HelpMen::report($movil->equipo_id,"\r\n :::::::::entranmdo por rta1:::::: ".$rta["rta"]);
                     if($rta["rta"]==1){
-                        HelpMen::report($movil->equipo_id,"\r\n :::::::::entranmdo por rta1:::::: ".$rta["rta"]);
+                        
                         self::updateSensores($movil,"iom",$perFieldInput,"",$rta,$posicion_id,$fecha);
                     }
                 }
