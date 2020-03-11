e0d2b5da575b16112f6327492021cd3e43589457
Alan Moratorio <amoratorio@siacseguridad.com>
2020-03-11 12:13:38 -0300

    log de pruebas en bitbio

--- app/Helpers/PerifericoHelp.php
+++ app/Helpers/PerifericoHelp.php
@@ -76,9 +76,9 @@ class PerifericoHelp {
     /*I3: Desenganche=>0 = ENGANCHADO; 1 = DESENGANCHADO |I1: Compuerta=>0 = CERRADA; 1 = ABIERTA*/
     public static function cambiosBitBIO($bioArr,$sensorEstado,$movil,$rta){
         //$rta         = array("rta"=>0,"estado_movil_id"=>$estado_movil_id,"tipo_alarma_id"=>0); //alarma_id=7 (Normal)
-        log::info(print_r($sensorEstado,true));
-        log::info(print_r($bioArr,true));
-        
+        //log::info(print_r($sensorEstado,true));
+        //log::info(print_r($bioArr,true));
+        Log::error("ahora está en BIO");
         HelpMen::report($movil->equipo_id,"*Evaluando cambios bit BIO* \r\n ");
         //self::actualizarPerifericosBIO($movil,$bioArr,$perFieldOutput);
         $estadoArr = explode(',',$sensorEstado);
